68bea35fe3ec7f7b3c8a7d38d5b2a7239d9e8415
chandanghosh <ckghosh1983@gmail.com>
2021-08-04 16:54:37 +0530

    Host information in netcore 5

---
+++ src/hostinfo/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Net;
+using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System.Linq;
+
+namespace hostinfo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(builder =>
+            {
+                builder.UseStartup<Program>();
+            }).Build().Run();
+        }
+
+        public void ConfigureServices(IServiceCollection services ) { }
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment environment)
+        {
+            app.UseRouting();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/", async ctx =>
+                {
+                    await ctx.Response.WriteAsync(
+                        $"  Framework - {System.Runtime.InteropServices.RuntimeInformation.FrameworkDescription}{Environment.NewLine}" +
+                        $"  OS - {System.Runtime.InteropServices.RuntimeInformation.RuntimeIdentifier}{Environment.NewLine}" +
+                        $"  Kernel - {System.Runtime.InteropServices.RuntimeInformation.OSDescription}{Environment.NewLine}" +
+                        $"  Hostname - {Environment.MachineName}{Environment.NewLine}" +
+                        $"  Host IP - {System.Net.Dns.GetHostEntry(Environment.MachineName).AddressList.FirstOrDefault(a => a.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)}"
+                    ).ConfigureAwait(false);
+                });
+            });
+        }
+
+    }
+}
