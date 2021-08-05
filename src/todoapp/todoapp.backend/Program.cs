a52b559fba88467edff59e99b5bc1912117f69ce
chandanghosh <ckghosh1983@gmail.com>
2021-08-05 17:45:55 +0530

    docker-compose files moved to docker folder and source code folder restructured

---
+++ src/todoapp/todoapp.backend/Program.cs
@@ -0,0 +1,104 @@
+﻿using Couchbase.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using todoapp.backend.Models;
+
+namespace todoapp.backend
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = CreateHostBuilder(args).Build();
+            // Todo: Refactor to a database seeder
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                var todoRepo = services.GetRequiredService<ITodoRepository>();
+                if (todoRepo.GetAll().Count is 0)
+                {
+                    todoRepo.AddNew(new TodoItem { Id = Guid.NewGuid().ToString(), Completed = false, Title = "Let's learn docker and kuberenetes!", Order = 3 });
+                    todoRepo.AddNew(new TodoItem { Id = Guid.NewGuid().ToString(), Completed = true, Title = "Check the new DotNet5!", Order = 1 });
+                    todoRepo.AddNew(new TodoItem { Id = Guid.NewGuid().ToString(), Completed = false, Title = "Learn another No-Sql database - Couchbase!", Order = 2 });
+                }
+
+            }
+            host.Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            return Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Program>();
+                });
+        }
+
+
+        public Program(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+
+            services.AddCors(o => o.AddDefaultPolicy(builder =>
+            {
+                builder.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader();
+            }));
+
+            services.AddCouchbase(x =>
+            {
+                // Change it to http://localhost:8091 to run locally
+                var DB_SERVER = Environment.GetEnvironmentVariable("DB_SERVER");
+                if (String.IsNullOrWhiteSpace(DB_SERVER))
+                {
+                    DB_SERVER = "localhost";
+                }
+
+                Console.WriteLine($"**************** DB SERVER is - {DB_SERVER} *******************");
+                x.Servers = new List<Uri> { new Uri($"http://{DB_SERVER}:8091") };
+                x.Username = "Administrator";
+                x.Password = "password";
+            });
+            services.AddTransient<ITodoRepository, TodoRepository>();
+
+            // Register Swagger generator
+            services.AddSwaggerGen();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Todo App V1");
+            });
+            app.UseRouting();
+
+            app.UseCors();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
---
+++ src/todoapp/todoapp.backend/ViewModels/TodoItemView.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using todoapp.backend.Models;
+
+namespace todoapp.backend.ViewModels
+{
+    public class TodoItemView
+    {
+        public TodoItemView(TodoItem item, HttpRequest req)
+        {
+            Title = item.Title;
+            Completed = item.Completed ?? false;
+            Url = req.Scheme + "://" + req.Host + "/" + item.Id;
+            Order = item.Order;
+        }
+
+        public string Title { get; }
+        public bool Completed { get; }
+        public string Url { get; }
+        public int? Order { get; }
+    }
+}
